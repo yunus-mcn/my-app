ac509505ab1dbfb1e0008bd573a424bb3f2b81e5
Yunus Macan <38140159+yunus-mcn@users.noreply.github.com>
2023-08-13 21:33:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
     images: {
       domains: ['cdn2.thecatapi.com'],
     },
+    cookie: {
+      // Çerezlerin şifrelenmesi
+      secret: 'YOUR_SECRET_KEY',
+  
+      // Çerezlerin geçerlilik süresi
+      expires: '1y',
+  
+      // Çerezlerin etki alanı
+      httpOnly: true,
+  
+      // Çerezlerin üçüncü taraf siteler tarafından okunması
+      secure: true,
+    },
   };
   
   module.exports = nextConfig;
